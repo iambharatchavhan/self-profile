63618c48acd08aff3fa3e276c4715daf1f1cc34e
iambharatchavhan <bharatchavhan141@gmail.com>
2024-01-21 18:15:50 +0530

    contact me page added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,19 +1,59 @@
-import React from 'react'
-import bg from "../assets/contactme.svg"
-import "./contact.css"
+import React from "react";
+import bg from "../assets/contactme.svg";
+import "./contact.css";
+import { MdEmail } from "react-icons/md";
+import { IoMdCall, IoIosHome ,IoLogoGithub,IoLogoLinkedin} from "react-icons/io";
+import { SiHashnode } from "react-icons/si";
 
 export default function Contact() {
   return (
-  <section id='contact'>
-    <div className='contact-container'>
-        <div className='left'>
-            <h2 className='contact-me-title'>Contact Me</h2>
-         <img src={bg} alt='contactImg'/>
+    <section id="contact">
+      <div className="contact-container">
+        <div className="contact-card">
+        <div className="left">
+          <h2 className="contact-me-title">Contact Me</h2>
+          <img src={bg} alt="contactImg" />
         </div>
-        <div className='right'>
-          Hello
+        <div className="right">
+          <div className="contact-details">
+            <div className="icon">
+              <MdEmail />
+            </div>
+            <div className="information">bharat.chavhan.1026@gmail.com</div>
+          </div>
+          <div className="contact-details">
+            <div className="icon">
+              <IoMdCall />
+            </div>
+            <div className="information" >+91 902 818 1744</div>
+          </div>
+          <div className="contact-details">
+            <div className="icon">
+              <IoIosHome />
+            </div>
+            <div className="information" >Pune, Maharashtra</div>
+          </div>
+          <div className="contact-details">
+            <div className="icon">
+              <IoLogoGithub />
+            </div>
+            <div className="information" >iambharatchavhan</div>
+          </div>
+          <div className="contact-details">
+            <div className="icon">
+              <IoLogoLinkedin />
+            </div>
+            <div className="information" >bharat-chavhan-563a881ba</div>
+          </div>
+          <div className="contact-details">
+            <div className="icon">
+              < SiHashnode />
+            </div>
+            <div className="information" >hashnode.com/@iambharat</div>
+          </div>
         </div>
-    </div>
-  </section>
-  )
+        </div>
+      </div>
+    </section>
+  );
 }
