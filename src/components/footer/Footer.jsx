dc4b24c89f2175fbeac724dc045d26e28ee126ac
iambharatchavhan <bharatchavhan141@gmail.com>
2024-01-23 15:57:29 +0530

    final call

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import "./footer.css"
 
 export default function Footer() {
   return (
 <section id='footer'>
- <h1>Footer</h1>
+   <div className='footer-details'>
+     <div>Email: bharat.chavhan.1026@gmail.com</div>
+     <div>Mobile: +91 908 818 1744</div>
+     <div>Address: Pune, Maharashtra</div>
+
+   </div>
+   <div className='copy'>BharatRChavhan©2024</div>
 </section>
   )
 }
